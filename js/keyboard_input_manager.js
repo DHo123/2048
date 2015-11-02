b576477d33a72fe2c431942f62b0ff66468b1f8c
calvwng <calvwng@gmail.com>
2015-11-02 03:23:27 -0800

    Refactored Voice4Game library and usage in 2048
    * Speech recognition logic is now within the Voice4Game object itself,
    rather than outside the object.
    
    * Transcription and log text destinations are now dynamic (currently can
    be removed or added through init to give user the option of not
    modifying HTML DOM).
    
    * 2048 now uses Voice4Game voice command registration for all voice
    commands.

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -50,231 +50,118 @@ KeyboardInputManager.prototype.listen = function () {
     65: 3  // A
   };
 
-  // Respond to voice commands
-  window.SpeechRecognition = window.SpeechRecognition       ||
-                             window.webkitSpeechRecognition ||
-                             null;
-
-  var stop = 0;
-  if (window.SpeechRecognition === null) {
-      document.getElementById('ws-unsupported').classList.remove('hidden');
-      document.getElementById('button-play-ws').setAttribute('disabled', 'disabled');
-      document.getElementById('button-stop-ws').setAttribute('disabled', 'disabled');
+  function mov(dir) {
+    return function() {
+      self.emit("move", dir);
+    };
   }
-  else {
-      var recognizer = new window.SpeechRecognition();
-      var transcription = document.getElementById('transcription');
-      var log = document.getElementById('log');
-      var snd = new Audio("file.wav");
 
-      // Interim results always on for demo purposes.
-      recognizer.interimResults = true;
+  Voice4Game.init({
+    'up': mov(0),
+    'right': mov(1),
+    'down': mov(2)
+  }, true);
 
-      recognizer.continuous = false;
+  Voice4Game.registerVoiceCmd('left', mov(3));
 
-      // Start recognising
-      recognizer.onresult = function(event) {
-          transcription.textContent = '';
+  Voice4Game.registerVoiceCmd('go down over', function(result) {
+      var durationMatches = result.match(/[0-9]+$/);
+      if (durationMatches.length > 0) {
+         var duration = parseInt(durationMatches[0]);
+         $('html, body').animate({
+           'scrollTop': $(document).height()
+         }, duration);
+      }
+  });
+  Voice4Game.registerVoiceCmd('go up over', function(result) {
+      var durationMatches = result.match(/[0-9]+$/);
+      if (durationMatches.length > 0) {
+         var duration = parseInt(durationMatches[0]);
+         $('html, body').animate({
+           'scrollTop': 0
+         }, duration);
+      }
+  });
+  Voice4Game.registerVoiceCmd('go down by', function(result) {
+      var offsetMatches = result.match(/[0-9]+$/);
+      if (offsetMatches.length > 0) {
+         var offset = parseInt(offsetMatches[0]);
+         $('html, body').animate({
+           'scrollTop': window.scrollY + offset
+         }, offset);
+      }
+  });
+  Voice4Game.registerVoiceCmd('go up by', function(result) {
+      var offsetMatches = result.match(/[0-9]+$/);
+      if (offsetMatches.length > 0) {
+         var offset = parseInt(offsetMatches[0]);
+         $('html, body').animate({
+           'scrollTop': window.scrollY - offset
+         }, offset);
+      }
+  });
 
-          var result;
+ /* Voice commands for page zoom control
+    1. Zoom in/out by the default 10% or a specified offset
+    2. Zoom to a specified absolute percentage
+    3. Reset zoom to 100% */
+ var zoomFunc = function(result) {
+  var curZoom = parseFloat(document.body.style.zoom);
+     var zoomStep = 10;               // 10% step by default
+     var numericMatches = result.match(/[0-9]+$/);
 
-          // Loop to find final result.
-          for (var i = event.resultIndex; i < event.results.length; i++) {
-              result = event.results[i][0].transcript;
+     if (isNaN(curZoom)) {
+        curZoom = 1.0;
+     }
+     if (numericMatches != null && numericMatches.length > 0) {
+        zoomStep = parseFloat(numericMatches[0]);
+     }
 
-              // Stops recognizing when "stop" is potentially heard
-              if (result.indexOf("stop") > -1 && stop == 0) {
-                  recognizer.stop();
-                  result = result.substring(0, result.indexOf("stop"));
-                  log.innerHTML = 'Recognition stopped' + result + '<br />' + log.innerHTML;
-                  stop = 1;
-              }
+     if (result.indexOf("in") > -1) { // Zoom in
+        curZoom += zoomStep / 100;
+     }
+     else {                           // Zoom out
+        curZoom -= zoomStep / 100;
+     }
+     // alert("zooming to " + curZoom);
+     document.body.style.zoom = curZoom;
+ };
+ Voice4Game.registerVoiceCmd('zoom in', zoomFunc);
+ Voice4Game.registerVoiceCmd('zoom out', zoomFunc);
+ Voice4Game.registerVoiceCmd('set zoom', function(result) {
+     var numericMatches = result.match(/[0-9]+$/);
+     if (numericMatches.length > 0) {
+        var newZoom = parseFloat(numericMatches[0]);
+        // alert("zooming to " + newZoom);
+        document.body.style.zoom = newZoom / 100;
+     }
+     else {
+        alert("set zoom: must specify percentage to zoom to");
+     }
+ });
+ Voice4Game.registerVoiceCmd('reset zoom', function() {
+     // alert("resetting zoom to 100%")
+     document.body.style.zoom = 1.0;
+ });
 
-              // Gets rid of weird extra whitespace.
-              if (i > 0 && stop == 0) {
-                console.log("test\n");
-                result = result.substring(1);
-              }
+ /* Voice commands for scrolling control
+    1. Scroll to top of the game container */
+ Voice4Game.registerVoiceCmd('scroll to game', function() {
+     var gameContainer = document.getElementsByClassName("game-container")[0];
+     var gameContainerY = gameContainer.getBoundingClientRect().top;
+     $('html, body').animate({
+        'scrollTop': gameContainerY
+     });
+ });
 
-              // When result is finalized
-              if (event.results[i].isFinal) {
-                transcription.textContent = result + ' (Confidence: ' + event.results[i][0].confidence + ')';
+  document.getElementById('button-play-ws').addEventListener('click', Voice4Game.start);
 
-                if (event.results[i][0].confidence < 0.5) {
-                      //Do something here if confidence is too low.
-                }
+  document.getElementById('button-stop-ws').addEventListener('click', Voice4Game.stop);
 
-                  // First word will be some command.
-                  var cmd = result.substr(0,result.indexOf(' '));
-
-                  if (result.indexOf("print") == 0) {
-                      log.innerHTML = result.substring(6) + '<br />' + log.innerHTML;
-                  }
-                  if (result == "hello") {
-                      alert("Hello to you as well.");
-                  }
-                  if (result.indexOf("new page") > -1) {
-                      window.open();
-                  }
-                  if (result.indexOf("go to ") > -1) {
-                      window.open("https://" + result.substring(6), "_blank");
-                  }
-                  else if (result.indexOf("goto ") > -1) {
-                      window.open("https://" + result.substring(5), "_blank");
-                  }
-
-                  // 2048 Commands
-                  if (result.indexOf("up") > -1) {
-                      self.emit("move", 0);
-                  }
-                  if (result.indexOf("right") > -1) {
-                      self.emit("move", 1);
-                  }
-                  if (result.indexOf("down") > -1) {
-                      self.emit("move", 2);
-                  }
-                  if (result.indexOf("left") > -1) {
-                      self.emit("move", 3);
-                  }
-                  if (result.indexOf("restart") > -1 ||
-                      result.indexOf("retry") > -1) {
-                      self.emit("restart");
-                  }
-                  if (result.indexOf("continue") > -1) {
-                      self.emit("keepPlaying");
-                  }
-
-                   /* Voice commands for scrolling control
-+                       1. Scroll to bottom/top over a specified ms duration
-+                       2. Scroll down/up by a specified pixel offset */
-                   if (result.indexOf("go down over") > -1) {
-                       var durationMatches = result.match(/[0-9]+$/);
-                       if (durationMatches.length > 0) {
-                          var duration = parseInt(durationMatches[0]);
-                          $('html, body').animate({
-                            'scrollTop': $(document).height()
-                          }, duration);
-                       }
-                   }
-                   if (result.indexOf("go up over") > -1) {
-                       var durationMatches = result.match(/[0-9]+$/);
-                       if (durationMatches.length > 0) {
-                          var duration = parseInt(durationMatches[0]);
-                          $('html, body').animate({
-                            'scrollTop': 0
-                          }, duration);
-                       }
-                   }
-                   if (result.indexOf("go down by") > -1) {
-                       var offsetMatches = result.match(/[0-9]+$/);
-                       if (offsetMatches.length > 0) {
-                          var offset = parseInt(offsetMatches[0]);
-                          $('html, body').animate({
-                            'scrollTop': window.scrollY + offset
-                          }, offset);
-                       }
-                   }
-                   if (result.indexOf("go up by") > -1) {
-                       var offsetMatches = result.match(/[0-9]+$/);
-                       if (offsetMatches.length > 0) {
-                          var offset = parseInt(offsetMatches[0]);
-                          $('html, body').animate({
-                            'scrollTop': window.scrollY - offset
-                          }, offset);
-                       }
-                   }
-                  
-                  /* Voice commands for page zoom control
-                     1. Zoom in/out by the default 10% or a specified offset
-                     2. Zoom to a specified absolute percentage
-                     3. Reset zoom to 100% */
-                  if (result.indexOf("zoom in") > -1 || result.indexOf("zoom out") > -1) {
-                      var curZoom = parseFloat(document.body.style.zoom);
-                      var zoomStep = 10;               // 10% step by default
-                      var numericMatches = result.match(/[0-9]+$/);
-
-                      if (isNaN(curZoom)) {
-                         curZoom = 1.0;
-                      }
-                      if (numericMatches != null && numericMatches.length > 0) {
-                         zoomStep = parseFloat(numericMatches[0]);
-                      }
-
-                      if (result.indexOf("in") > -1) { // Zoom in
-                         curZoom += zoomStep / 100;
-                      }
-                      else {                           // Zoom out
-                         curZoom -= zoomStep / 100;
-                      }
-                      // alert("zooming to " + curZoom);
-                      document.body.style.zoom = curZoom;
-                  }
-                  if (result.indexOf("set zoom ") > -1) {
-                      var numericMatches = result.match(/[0-9]+$/);
-                      if (numericMatches.length > 0) {
-                         var newZoom = parseFloat(numericMatches[0]);
-                         // alert("zooming to " + newZoom);
-                         document.body.style.zoom = newZoom / 100;
-                      }
-                      else {
-                         alert("set zoom: must specify percentage to zoom to");
-                      }
-                  }
-                  if (result.indexOf("reset zoom") > -1) {
-                      // alert("resetting zoom to 100%")
-                      document.body.style.zoom = 1.0;
-                  }
-
-                  /* Voice commands for scrolling control
-                     1. Scroll to top of the game container */
-                  if (result.indexOf("scroll to game") > -1) {
-                      var gameContainer = document.getElementsByClassName("game-container")[0];
-                      var gameContainerY = gameContainer.getBoundingClientRect().top;
-                      $('html, body').animate({
-                         'scrollTop': gameContainerY
-                      });
-                  }
-              }
-              else if(stop == 0) {
-                transcription.textContent += result;
-              }
-          }
-      };
-      // Listen for errors
-      recognizer.onerror = function(event) {
-          log.innerHTML = 'Recognition error: ' + event.error + '<br />' + log.innerHTML;
-      };
-
-      recognizer.onend = function(event) {
-          if (stop == 0) {
-            recognizer.start();
-          }
-      };
-
-      document.getElementById('button-play-ws').addEventListener('click', function() {
-          try {
-              recognizer.start();
-              // Play sound when started
-              snd.play();
-              stop = 0;
-              log.innerHTML = 'Recognition started' + '<br />' + log.innerHTML;
-          }
-          catch(ex) {
-                  log.innerHTML = 'Recognition error: ' + ex.message + '<br />' + log.innerHTML;
-          }
-      });
-
-      document.getElementById('button-stop-ws').addEventListener('click', function() {
-          stop = 1;
-          recognizer.stop();
-          log.innerHTML = 'Recognition stopped' + '<br />' + log.innerHTML;
-      });
-
-      document.getElementById('clear-all').addEventListener('click', function() {
-        transcription.textContent = '';
-        log.textContent = '';
-      });
-    }
+  document.getElementById('clear-all').addEventListener('click', function() {
+    transcription.textContent = '';
+    log.textContent = '';
+  });
 
   // Respond to direction keys
   document.addEventListener("keydown", function (event) {
